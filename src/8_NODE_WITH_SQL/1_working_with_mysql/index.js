c78bf66a3e65fac5b1196d5e76f7aedce6c0523c
Douglas Tertuliano <douglasjtds@gmail.com>
2023-08-24 00:13:09 -0300

    insert books into table books

--- src/8_NODE_WITH_SQL/1_working_with_mysql/index.js
+++ src/8_NODE_WITH_SQL/1_working_with_mysql/index.js
@@ -4,6 +4,12 @@ const mysql2 = require('mysql2')
 
 const app = express()
 
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
@@ -11,6 +17,22 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.post('/books/insertbook', (req, res) => {
+
+    const title = req.body.title
+    const pageqty = req.body.pageqty
+
+    const sqlQuery = `INSERT INTO books (title, pageqty) VALUES ('${title}', '${pageqty}')`
+
+    conn.query(sqlQuery, function(err) {
+        if (err){
+            console.log(err)
+        }
+
+        res.redirect('/')
+    })
 })
 
 const conn = mysql2.createConnection({
