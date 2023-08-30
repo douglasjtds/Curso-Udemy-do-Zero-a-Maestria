4c854573a9b6dbf89d50172d5b38b406a40ee952
Douglas Tertuliano <douglasjtds@gmail.com>
2023-08-29 21:39:59 -0300

    starting with sequelize

---
+++ src/9_SEQUELIZE/1_working_with_sequelize/db/conn.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize')
+const sequelize = new Sequelize('nodesequelize', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+
+try {
+    sequelize.authenticate()
+    console.log('Succesfully connected with Sequelize.')
+} catch (err) {
+    console.log('Error to connect: ', err)
+}
+
+module.exports = sequelize
