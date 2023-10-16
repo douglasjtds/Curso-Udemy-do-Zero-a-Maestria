78b975b36e953327dd1ab4746a8bb24d5bc341c9
Douglas Tertuliano <douglasjtds@gmail.com>
2023-10-16 16:07:34 -0300

    feat: now its possible to search edit and remove thoughts

--- src/11_THOUGHTS/controllers/ThoughtController.js
+++ src/11_THOUGHTS/controllers/ThoughtController.js
@@ -1,9 +1,31 @@
 const Thought = require('../models/Thought')
 const User = require('../models/User')
 
+const { Op } = require('sequelize')
+
 module.exports = class ThoughtController {
     static async showThoughts (req, res) {
-        res.render('thoughts/home')
+        // check search input
+        let search = ''
+
+        if (req.query.search) {
+            search = req.query.search
+        }
+
+        const thoughtsData = await Thought.findAll({
+            include: User, 
+            where: {
+                title: { [Op.like]: `%${search}%` },
+            }
+        })
+        const thoughts = thoughtsData.map((result) => result.get({plain: true}))
+        let thoughtsQty = thoughts.length
+
+        if (thoughtsQty === 0) {
+            thoughtsQty = false
+        }
+
+        res.render('thoughts/home', { thoughts, search , thoughtsQty })
     }
 
     static async dashboard (req, res) {
@@ -24,7 +46,12 @@ module.exports = class ThoughtController {
 
         const thoughts = user.Thoughts.map((result) => result.dataValues)
 
-        res.render('thoughts/dashboard', {thoughts})
+        let emptyThoughts = false
+        if (thoughts.length === 0) {
+            emptyThoughts = true
+        }
+
+        res.render('thoughts/dashboard', {thoughts, emptyThoughts})
     }
 
     static createThought (req,res) {
@@ -48,5 +75,43 @@ module.exports = class ThoughtController {
         } catch (error) {
             console.log('An error occurred: ' + error)
         }
+    }
+
+    static async updateThought(req, res) {
+        const id = req.params.id
+        const thought = await Thought.findOne({where: { id: id }, raw: true})
+        res.render('thoughts/edit', {thought})
+    }
+
+    static async updateThoughtSave(req, res) {
+        const id = req.body.id
+        const thought = { title: req.body.title }
+
+        try {
+            await Thought.update(thought, {where: {id : id}})
+            req.flash('message', 'Thought sucessfully updated!')
+            req.session.save(() => {
+                res.redirect('/thoughts/dashboard')
+            })
+        } catch (error) {
+            console.log('An error occurred: ' + error)
+        }
+    }
+
+    static async removeThought(req, res) {
+        const id = req.body.id
+        const UserId = req.session.userid
+
+        try {
+            await Thought.destroy({where: {id: id, UserId: UserId}})
+
+            req.flash('message', 'Thought sucessfully removed!')
+            req.session.save(() => {
+                res.redirect('/thoughts/dashboard')
+            })
+        } catch (error) {
+            console.log('An error occurred: ' + error)
+        }
+
     }
 }
