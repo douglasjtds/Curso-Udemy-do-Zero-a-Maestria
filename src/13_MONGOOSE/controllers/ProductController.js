ab8e870cf2bb57eab324cdd299ce84f297e2bd93
Douglas Tertuliano <douglasjtds@gmail.com>
2023-11-02 13:02:18 -0300

    feat: converting raw mongo to mongoose

--- src/13_MONGOOSE/controllers/ProductController.js
+++ src/13_MONGOOSE/controllers/ProductController.js
@@ -2,41 +2,43 @@ const Product = require('../models/Product')
 
 module.exports = class ProductController {
     static async showProducts (req, res) {
-        const products = await Product.getProducts()
+        const products = await Product.find().lean()
+
         res.render('products/all', { products })
     }
 
     static createProduct(req, res) {
         res.render('products/create')
     }
-    static createProductPost (req, res) {
+
+    static async createProductPost (req, res) {
         const name = req.body.name
         const image = req.body.image
         const price = req.body.price
         const description = req.body.description
 
-        const product = new Product(name, image, price, description)
-        product.save()
+        const product = new Product({name, image, price, description})
+        await product.save()
         res.redirect('/')
     }
 
     static async getProduct(req, res) {
         const id = req.params.id
-        const product = await Product.getProductById(id)
-        // console.log(product)
+        const product = await Product.findById(id).lean()
+        console.log(product)
 
         res.render('products/product', { product })
     }
 
     static async deleteProduct (req, res) {
         const id = req.params.id
-        await Product.removeProductById(id)
+        await Product.deleteOne({ _id: id })
         res.redirect('/')
     }
     
     static async editProduct(req, res){
         const id = req.params.id
-        const product = await Product.getProductById(id)
+        const product = await Product.findById(id).lean()
         res.render('products/edit', { product })
     }
     
@@ -47,9 +49,9 @@ module.exports = class ProductController {
         const price = req.body.price
         const description = req.body.description
         
-        const product = new Product(name, image, price, description)
+        const product = { name, image, price, description }
         
-        product.updateProduct(id)
+        await Product.updateOne({ _id: id }, product)
         res.redirect('/')
     }
 }
