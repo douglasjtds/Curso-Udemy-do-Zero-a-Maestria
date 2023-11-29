e4e4994ee538a8e4ae6a459c9cd62a664e1c0238
Douglas Tertuliano <douglasjtds@gmail.com>
2023-11-28 22:27:33 -0300

    feat: get user adoptions

--- src/15_GET_A_PET/backend/routes/PetRoutes.js
+++ src/15_GET_A_PET/backend/routes/PetRoutes.js
@@ -13,6 +13,7 @@ router.post(
 );
 router.get('/', PetController.getAll)
 router.get('/mypets', verifyToken, PetController.getAllUserPets)
+router.get('/myadoptions', verifyToken, PetController.getAllUserAdoptions)
 
 
 module.exports = router;
