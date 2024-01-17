e78fdf6e7f7ec8d7a48419341cd9bfdb7099fa9b
Douglas Tertuliano <douglasjtds@gmail.com>
2024-01-16 21:30:36 -0300

    now were able to delete pet

--- src/15_GET_A_PET/backend/controllers/PetController.js
+++ src/15_GET_A_PET/backend/controllers/PetController.js
@@ -116,13 +116,41 @@ module.exports = class PetController {
 
     // check if pet exists
     const pet = await Pet.findOne({ _id: id });
-
     if (!pet) {
       res.status(404).json({ message: 'Pet not found!' });
     }
 
     res.status(200).json({
-        pet: pet,
-    })
+      pet: pet,
+    });
+  }
+
+  static async removePetById(req, res) {
+    const id = req.params.id;
+
+    if (!ObjectId.isValid(id)) {
+      res.status(422).json({ message: 'Invalid Id!' });
+      return;
+    }
+
+    // check if pet exists
+    const pet = await Pet.findOne({ _id: id });
+    if (!pet) {
+      res.status(404).json({ message: 'Pet not found!' });
+      return
+    }
+
+    // check if logged in user registered the pet
+    const token = getToken(req)
+    const user = await getUserByToken(token)
+    if (pet.user._id.toString() !== user._id.toString()) {
+      res.status(422).json({message: "There was an error processing your request, please try again later!"})
+      console.log("Logged user cannot delete this pet!")
+      return
+    }
+
+    await Pet.findByIdAndRemove(id)
+
+    res.status(200).json({message: "Pet successfully removed!"})
   }
 };
