7ffe5201abfdca53cd6400957cb29c04c924b204
Douglas Tertuliano <douglasjtds@gmail.com>
2024-03-26 23:44:14 -0300

    register user's page completely working

--- src/15_GET_A_PET/frontend/src/components/pages/Auth/Register.js
+++ src/15_GET_A_PET/frontend/src/components/pages/Auth/Register.js
@@ -1,7 +1,68 @@
+import Input from "../../form/Input"
+import styles from '../../form/Form.module.css'
+import { Link } from 'react-router-dom'
+import { useContext, useState } from 'react'
+
+/* contexts */
+import { Context } from "../../../context/UserContext"
+
 function Register () {
+    const [ user, setUser ] = useState({})
+    const {register} = useContext(Context)
+
+    function handleChange(e){
+        setUser({ ...user, [e.target.name]: e.target.value})
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+
+        // send user to database
+        register(user)
+    }
+
     return (
-        <section>
+        <section className={styles.form_container}>
             <h1>Register</h1>
+            <form onSubmit={handleSubmit}>
+                <Input
+                    text="Name"
+                    type="text"
+                    name="name"
+                    placeholder="Type your name"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="Phone"
+                    type="text"
+                    name="phone"
+                    placeholder="Type your phone"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="E-mail"
+                    type="email"
+                    name="email"
+                    placeholder="Type your e-mail"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="Password"
+                    type="password"
+                    name="password"
+                    placeholder="Type your password"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="Password confirmation"
+                    type="password"
+                    name="confirmpassword"
+                    placeholder="Confirm your password"
+                    handleOnChange={handleChange}
+                />
+                <input type="submit" value="Register"/>
+            </form>
+            <p>Already have an account? <Link to="/login">Click here.</Link></p>
         </section>
     )
 }
---
+++ src/15_GET_A_PET/frontend/src/hooks/useAuth.js
@@ -0,0 +1,46 @@
+import api from '../utils/api'
+import useFlashMessage from './useFlashMessage'
+import { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+
+export default function useAuth() {
+    const [ authenticated, setAuthenticated ] = useState(false)
+    const { setFlashMessage } = useFlashMessage()
+    const history = useHistory()
+
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+
+        if(token) {
+            api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`
+            setAuthenticated(true)
+        }
+    }, [])
+
+    async function register(user) {
+
+        let msgText = 'User successfully registered!'
+        let msgType = 'success'
+
+        try {
+            const data = await api.post('/users/register', user).then((response) => {
+                return response.data
+            })
+
+            await authUser(data)
+        } catch (error) {
+            msgText = error.response.data.message
+            msgType = 'error'
+        }
+
+        setFlashMessage(msgText, msgType)
+    }
+
+    async function authUser (data){ 
+        setAuthenticated(true)
+        localStorage.setItem('token', JSON.stringify(data.token))
+        history.push('/')
+    }
+
+    return { authenticated, register }
+}
